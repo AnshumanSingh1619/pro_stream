3de8ed50b9d8c0ead75cde1bdacf1458c03e93ff
Anshumansingho7 <anshumansinghshaktawat09@gmail.com>
2024-05-07 19:26:23 +0530

    css

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,3 @@ set -o errexit
 bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
-append :linked_dirs, 'storage', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads', 'node_modules'
