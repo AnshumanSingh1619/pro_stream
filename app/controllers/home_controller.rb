b587c7f35a59e88d4c378771ecc5a78eb16fef91
Anshumansingho7 <anshumansinghshaktawat09@gmail.com>
2024-05-15 16:53:08 +0530

    css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
 class HomeController < ApplicationController
   before_action :authenticate
-  before_action :set_content, only: [:showcontent, :showseason, :seasondestroy, :episodedestroy]
-  before_action :set_season, only: [:showseason, :seasondestroy, :episodedestroy,]
-  before_action :set_episode, only: [:episodedestroy,]
+  before_action :set_content, only: [:showcontent, :showseason]
+  before_action :set_season, only: [:showseason]
   include ApplicationHelper
 
   def index
@@ -16,13 +15,18 @@ class HomeController < ApplicationController
   end
 
   def seasondestroy
-    @season.destroy
-    redirect_to content_path(@content), notice: "Season was successfully destroyed."
+    content = @mcontents.find(params[:content_id])
+    season = content.seasons.find(params[:season_id])
+    season.destroy
+    redirect_to content_path(content), notice: "Season was successfully destroyed."
   end
 
   def episodedestroy
-    @episode.destroy
-    redirect_to content_path(@content), notice: "Episode was successfully destroyed."
+    content = @mcontents.find(params[:content_id])
+    season = content.seasons.find(params[:season_id])
+    episode = season.episodes.find(params[:episode_id])
+    episode.destroy
+    redirect_to content_path(content), notice: "Episode was successfully destroyed."
   end
 
   private
@@ -33,10 +37,6 @@ class HomeController < ApplicationController
 
   def set_season
     @season = @content.seasons.find(params[:season_id])
-  end
-
-  def set_episode
-    @episode = @season.episodes.find(params[:episode_id])
   end
 
   def authenticate 
