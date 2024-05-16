ee4749fd5efd4f819666c34cc32dce7f35053506
Anshumansingho7 <anshumansinghshaktawat09@gmail.com>
2024-05-16 18:07:05 +0530

    css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if resource.is_a?(User)
       semi_users_path
+    elsif resource.is_a?(Admin)
+      contents_url
     else
       super
     end
