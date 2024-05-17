e8847d6fe143200facc4dc586920d6f0fee2fc71
Anshumansingho7 <anshumansinghshaktawat09@gmail.com>
2024-05-17 11:00:25 +0530

    first

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -11,7 +11,6 @@ class Users::SessionsController < Devise::SessionsController
   # POST /resource/sign_in
   def create
     super
-    UserMailer.registration().deliver_now
   end
 
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,2 @@
 class UserMailer < ApplicationMailer
-
-  def registration()
-    email = "ashaktawat43@gmail.com"
-    mail(to: email, subject: 'Welcome mail')
-  end
-
 end
