6912a6620152012e453a820a5760fcb05a64f924
Anshumansingho7 <anshumansinghshaktawat09@gmail.com>
2024-06-05 13:52:05 +0530

    sidekiq

--- app/sidekiq/destroy_admin_otp_job.rb
+++ app/sidekiq/destroy_admin_otp_job.rb
@@ -3,8 +3,7 @@ class DestroyAdminOtpJob
 
   def perform(admin_id)
     admin = Admin.find_by(id: admin_id)
-    return unless admin 
-
+    
     if admin.otp == 0
       admin.update(otp: 11)
     end
