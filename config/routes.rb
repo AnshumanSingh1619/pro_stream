5e8b7a3f9f1973716dcb0b0b52735122a727b4b5
Anshumansingho7 <anshumansinghshaktawat09@gmail.com>
2024-06-13 19:00:27 +0530

    responsive

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,14 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations'
   }  
   devise_for :admins, controllers: {
     sessions: 'admins/sessions',
     passwords: 'admins/passwords',
-    registrations: 'admins/registrations'
+    registrations: 'admins/registrations',
+    confirmations: 'admins/confirmations'
   }
   resources :semi_users
   root to: "home#index"
